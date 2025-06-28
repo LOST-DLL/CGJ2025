f6d879e611d6a547b9a2a130618417cb7e6540c1
LOST-DLL <3103736510@qq.com>
2025-06-28 15:51:33 +0800

    2.5D 但是用3D碰撞体

--- Assets/Scripts/PlayerControllerBase.cs
+++ Assets/Scripts/PlayerControllerBase.cs
@@ -7,7 +7,7 @@ using UnityEngine.Animations;
 public class PlayerControllerBase : MonoBehaviour
 {
     public float speed = 5;
-    new private Rigidbody2D rigidbody;
+    new private Rigidbody rigidbody;
     private Animator animator;
     private float inputX, inputY;
 
@@ -18,7 +18,7 @@ public class PlayerControllerBase : MonoBehaviour
 
     protected void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
     }
 
