5325a7cc5281daa5e50aa3d144b337775d5cd219
Myian <161828864+Yi-noah@users.noreply.github.com>
2025-06-29 05:04:16 +0800

    UI8
    888888888

--- Assets/Scripts/SceneChangeTmp.cs
+++ Assets/Scripts/SceneChangeTmp.cs
@@ -4,20 +4,48 @@ using UnityEngine;
 
 public class SceneChangeTmp : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] player1Prefabs;  // 存储所有玩家1对应的预制件
+    public GameObject[] player2Prefabs;  // 存储所有玩家2对应的预制件
+
+    // 直接引用玩家1和玩家2的控制器
+    public PlayerControllerBase player1;  // 玩家1的控制器
+    public PlayerControllerBase player2;  // 玩家2的控制器
+
+    private int player1Index;
+    private int player2Index;
+
+
+    void Awake()
+    {
+        // 从 PlayerPrefs 中读取索引
+        player1Index = PlayerPrefs.GetInt("Player1Index", 0);  // 默认值为 0
+        player2Index = PlayerPrefs.GetInt("Player2Index", 0);  // 默认值为 0
+
+        // 根据索引实例化玩家1和玩家2的预制件
+        InstantiateCharacter(player1Index, player1Prefabs, player1);
+        InstantiateCharacter(player2Index, player2Prefabs, player2);
+    }
     void Start()
     {
-        int player1Index = PlayerPrefs.GetInt("Player1Index", 0);  // 第二个参数是默认值，如果没有找到保存的值则使用0
-        int player2Index = PlayerPrefs.GetInt("Player2Index", 0);
-
-        // 打印 player1Index，确保读取正确
-        Debug.Log("Player 1's index in this scene: " + player1Index);
-        Debug.Log("Player 2's index in this scene: " + player2Index);
+        
     }
 
-    // Update is called once per frame
-    void Update()
+    // 根据索引实例化玩家的预制件
+    void InstantiateCharacter(int index, GameObject[] prefabs, PlayerControllerBase player)
     {
-        
+        // 确保索引不越界
+        if (index >= 0 && index < prefabs.Length)
+        {
+            Debug.Log("name is: " + prefabs[index].name);
+            // 获取预制件的名称并赋值给玩家的 Character 属性
+            string prefabName = prefabs[index].name;  // 获取预制件的名称
+
+            // 将名称赋值给玩家的 characterName 属性
+            player.characterName = prefabName;
+        }
+        else
+        {
+            Debug.LogWarning("Index out of bounds for prefabs array: " + index);
+        }
     }
 }
