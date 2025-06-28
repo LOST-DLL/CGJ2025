4cc5e1ec5be2b002578c4bde33bb49ffb24f74c0
Myian <161828864+Yi-noah@users.noreply.github.com>
2025-06-28 22:36:57 +0800

    Update ProjectVersion.txt
    5555

--- Assets/Scripts/SceneChangeTmp.cs
+++ Assets/Scripts/SceneChangeTmp.cs
@@ -4,20 +4,39 @@ using UnityEngine;
 
 public class SceneChangeTmp : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] player1Prefabs;  // 存储所有玩家1对应的预制件
+    public GameObject[] player2Prefabs;  // 存储所有玩家2对应的预制件
+
+    // 直接引用玩家1和玩家2的出生点位置
+    public Transform player1SpawnPoint;  // 玩家1的出生点
+    public Transform player2SpawnPoint;  // 玩家2的出生点
+
+    private int player1Index;
+    private int player2Index;
+
     void Start()
     {
-        int player1Index = PlayerPrefs.GetInt("Player1Index", 0);  // 第二个参数是默认值，如果没有找到保存的值则使用0
-        int player2Index = PlayerPrefs.GetInt("Player2Index", 0);
+        // 从 PlayerPrefs 中读取索引
+        player1Index = PlayerPrefs.GetInt("Player1Index", 0);  // 默认值为 0
+        player2Index = PlayerPrefs.GetInt("Player2Index", 0);  // 默认值为 0
 
-        // 打印 player1Index，确保读取正确
-        Debug.Log("Player 1's index in this scene: " + player1Index);
-        Debug.Log("Player 2's index in this scene: " + player2Index);
+        // 根据索引实例化玩家1和玩家2的预制件
+        InstantiateCharacter(player1Index, player1Prefabs, player1SpawnPoint);
+        InstantiateCharacter(player2Index, player2Prefabs, player2SpawnPoint);
     }
 
-    // Update is called once per frame
-    void Update()
+    // 根据索引实例化玩家的预制件
+    void InstantiateCharacter(int index, GameObject[] prefabs, Transform spawnPoint)
     {
-        
+        // 确保索引不越界
+        if (index >= 0 && index < prefabs.Length)
+        {
+            // 实例化预制件到指定位置
+            Instantiate(prefabs[index], spawnPoint.position, spawnPoint.rotation);
+        }
+        else
+        {
+            Debug.LogWarning("Index out of bounds for prefabs array: " + index);
+        }
     }
 }
